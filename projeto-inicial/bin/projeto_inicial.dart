48c596a523beba2f330c93b1e821f3f43b1ec561
Matheus Hora <matheus11hora@gmail.com>
2022-10-24 17:54:09 -0300

    Novos

--- projeto-inicial/bin/projeto_inicial.dart
+++ projeto-inicial/bin/projeto_inicial.dart
@@ -1,9 +1,39 @@
 void main() {
   int idade = 20;
   double altura = 1.70;
-  bool geek = true;
-  String nome = 'Matheus Hora';
-  String apelido = 'Hora';
-  var frase = 'Eu sou o $apelido \n mas meu nome completo é: $nome \n ';
+
+  //Serve para declarar sozinho o tipo(quando nao sabe o tipo de resposta que vai receber)
+  var geek = true;
+
+  //const so pode ser declarada com as informações
+  const String nome = 'Matheus Hora';
+
+  //final pode ser declarada quando quiser e começar sem valor, e so pode ter uma infomação
+  final String apelido = 'Hora';
+
+  // print(frase);
+
+  bool maiorDeIdade;
+
+  if(idade >= 18){
+    maiorDeIdade = true;
+  } else {
+    maiorDeIdade = false;
+  }
+
+  String nome1 = 'a';
+  String nome2 = 'b';
+  String nome3 = 'c';
+  String nome4 = 'd';
+
+  List<String> listanomes = ['a', 'b', 'c', 'd', 'e', 'f'];
+  List hora = ['Mathues Hora', 'Hora', 20, 1.70, true];
+
+  String frase = 'Eu sou ${hora[1]} \n'
+      'mas meu nome completo é: ${hora[0]}, \n'
+      'eu me considero geek? ${hora[4]}. \n'
+      'Eu tenho ${hora[3]} metros de altura e \n'
+      '${hora[2]} anos de idade,\n'
+      'Eu sou maior de idade? $maiorDeIdade';
   print(frase);
 }
