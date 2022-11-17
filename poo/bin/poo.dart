2988543db44d47473ac153ca7ecae4ab45d45bcb
Matheus Hora <matheus11hora@gmail.com>
2022-11-16 22:19:18 -0300

    Novos

--- poo/bin/poo.dart
+++ poo/bin/poo.dart
@@ -1,3 +1,14 @@
 void main() {
-  String nome = 'Laranja';
+  sobeLaranja('laranja', 98, 20);
+}
+
+void sobeLaranja(String nome, double peso, int dias, {bool? isMadura}) {
+  int diasParaColher = 30;
+  isMadura = dias >= diasParaColher;
+
+  if (!isMadura) {
+    print(
+      'A $nome pesa $peso gramas! Ela foi colhida há $diasParaColher dias e precisar de $dias para amadurecer, logo, a Laranja está $isMadura',
+    );
+  }
 }
