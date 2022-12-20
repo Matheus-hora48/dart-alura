dc74dbfb79160b482bc010a05a21db7cb541634d
Matheus Hora <matheus11hora@gmail.com>
2022-12-19 23:44:03 -0300

    Novos

--- poo/bin/poo.dart
+++ poo/bin/poo.dart
@@ -1,5 +1,14 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 void main() {
   sobeLaranja('laranja', 98, 20);
+
+  Fruta fruta01 = Fruta(
+    nome: 'Laranja',
+    peso: 34,
+    cor: 'Laranja',
+    sabor: 'Doce',
+    diasDesdeColheita: 10,
+  );
 }
 
 void sobeLaranja(String nome, double peso, int dias, {bool? isMadura}) {
@@ -10,5 +19,27 @@ void sobeLaranja(String nome, double peso, int dias, {bool? isMadura}) {
     print(
       'A $nome pesa $peso gramas! Ela foi colhida há $diasParaColher dias e precisar de $dias para amadurecer, logo, a Laranja está $isMadura',
     );
+  }
+}
+
+class Fruta {
+  String nome;
+  double peso;
+  String cor;
+  String sabor;
+  int diasDesdeColheita;
+  bool? isMadura;
+
+  Fruta({
+    required this.nome,
+    required this.peso,
+    required this.cor,
+    required this.sabor,
+    required this.diasDesdeColheita,
+    this.isMadura,
+  });
+
+  estaMadura(int diasDesdeColheita) {
+    isMadura = diasDesdeColheita >= diasDesdeColheita;
   }
 }
